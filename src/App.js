052f56ae395d8162439ad8ef55e03ae31d548e6f
Nico <info@synnovation-inc.de>
2025-09-30 19:01:24 +0200

    fd

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import PVConfigurator from './components/PVConfigurator';
 import Settings from './components/Settings';
 import UserAvatar from './components/UserAvatar';
 import Sidebar from './components/Sidebar';
+import AdminSetup from './components/AdminSetup';
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = React.useState(true);
@@ -36,6 +37,7 @@ function App() {
                   <div className="min-h-dvh bg-gray-50">
                     <Routes>
                       <Route path="/login" element={<Login />} />
+                      <Route path="/admin-setup" element={<AdminSetup />} />
                       <Route 
                         path="/*" 
                         element={
